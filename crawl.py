49a511b5353f24eac119f95336508879e803378f
Aman Rawat <kirmolaa@gmail.com>
2024-07-05 17:43:44 +0530

    initial commit for html to text extract to csv

--- crawl.py
+++ crawl.py
@@ -8,9 +8,34 @@ def parse_html(file_path):
         content = file.read()
         soup = BeautifulSoup(content, 'html.parser')
         # Extract data from the HTML file, customize this as per your needs
-        title = soup.title.string if soup.title else 'No Title'
+        title = soup.title.string if soup.title else ''
+        meta_desc = soup.find("meta", attrs={
+            "name":"description"
+        })["content"]
+        
+        # remove next pages
+        content = soup.find("div", attrs={
+            "id":"city"
+        })
+        
+        next_top = content.find("div", attrs={
+            "id":'bottomnextup',
+        })
+        next_bottom = content.find("div", attrs={
+            "id":'bottomnext',
+        })
+        next_topic_div = content.find("div", attrs={
+            "class":'nexttopicdiv',
+        })
+        next_top.extract()
+        next_bottom.extract()
+        next_topic_div.extract()
         # Add more fields as required
-        return {'file': file_path, 'title': title}
+        return {
+            'title': title,
+            "meta_desc": meta_desc,
+            "body": content
+        }
 
 def main(folder_path, output_csv):
     html_files = [os.path.join(folder_path, f) for f in os.listdir(folder_path) if f.endswith('.html')]
@@ -23,6 +48,6 @@ def main(folder_path, output_csv):
     df.to_csv(output_csv, index=False)
 
 if __name__ == '__main__':
-    folder_path = 'path/to/your/html/folder'  # Specify the folder containing HTML files
-    output_csv = 'output.csv'  # Specify the output CSV file name
+    folder_path = './blog'  # Specify the folder containing HTML files
+    output_csv = 'result/output.csv'  # Specify the output CSV file name
     main(folder_path, output_csv)
